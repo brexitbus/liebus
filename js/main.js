ff9d71f61ef11419c37f30a2be8144333e6d31e8
brexitbus <50377252+brexitbus@users.noreply.github.com>
2019-05-07 19:28:01 +0100

    twks

--- js/main.js
+++ js/main.js
@@ -8,10 +8,10 @@ $(function() {
 
     console.log('vars', vars);
 
-    $('.line-1').html(decodeURIComponent( vars.line1.replace('+', ' ') ));
-    $('[name="line1"]').val(decodeURIComponent(vars.line1.replace('+', ' ')));
-    $('.line-2').html(decodeURIComponent(vars.line2.replace('+', ' ')));
-    $('[name="line2"]').val(decodeURIComponent(vars.line2.replace('+', ' ')));
-    $('.line-3').html(decodeURIComponent(vars.line3.replace('+', ' ')));
-    	$('[name="line3"]').val(decodeURIComponent(vars.line3.replace('+', ' ')));
+    $('.line-1').html(decodeURIComponent( vars.line1.replace(/\+/g, ' ') ));
+    $('[name="line1"]').val(decodeURIComponent(vars.line1.replace(/\+/g, ' ')));
+    $('.line-2').html(decodeURIComponent(vars.line2.replace(/\+/g, ' ')));
+    $('[name="line2"]').val(decodeURIComponent(vars.line2.replace(/\+/g, ' ')));
+    $('.line-3').html(decodeURIComponent(vars.line3.replace(/\+/g, ' ')));
+    	$('[name="line3"]').val(decodeURIComponent(vars.line3.replace(/\+/g, ' ')));
 });
